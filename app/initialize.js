435153ea6e46d2715dc01600294519c8f26f3f9a
Anton Oparin <mavarazy@gmail.com>
2017-04-06 21:00:15 +0200

    Adding google analytics

--- app/initialize.js
+++ app/initialize.js
@@ -3,15 +3,21 @@ import {applyMiddleware, createStore} from "redux";
 import thunk from "redux-thunk";
 import {composeWithDevTools} from "redux-devtools-extension";
 import {browserHistory} from "react-router";
-import authService from "service/auth";
 import {syncHistoryWithStore} from "react-router-redux";
 import React from "react";
 import ReactDOM from "react-dom";
 import MainApp from "./route";
 import {Provider} from "react-redux";
+
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-96949345-1');
+
 const store = createStore(reducers(), composeWithDevTools(applyMiddleware(thunk)));
 
 const history = syncHistoryWithStore(browserHistory, store);
+history.listen(function (location) {
+    window.ga('send', 'pageview', location.pathname);
+});
 
 const load = () => {
     ReactDOM.render(
