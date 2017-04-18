311ae804aed9b413e0dc432fb2ad38eef65a4dc6
Anton Oparin <mavarazy@gmail.com>
2017-04-18 15:54:10 +0300

    Updating contributor

--- app/components/landing/Contributor.jsx
+++ app/components/landing/Contributor.jsx
@@ -1,19 +1,18 @@
 import React, {Component} from "react";
 
-export default class Contributor extends Component {
+import ComponentWrap from "../ComponentWrap";
+
+class ContributorHero extends Component {
     render() {
         return (
-            <section className="hero">
+            <section className="hero is-primary is-large header-image contributor-image">
                 <div className="has-text-centered">
-                    <div className="columns is-vcentered">
-                        <div className="column ">
-                            <img src="/images/contributor.jpg"></img>
-                        </div>
-                        <div className="column">
-                            <h1 className="title is-2">
+                    <div className="hero-body">
+                        <div className="container has-text-centered">
+                            <h1 className="title is-1">
                                 Giver
                             </h1>
-                            <h2 className="subtitle is-5">
+                            <h2 className="subtitle is-3">
                                 Great artist does not need to die poor and unrecognized<br/>
                                 Something anyone can spare<br/>
                             </h2>
@@ -21,6 +20,91 @@ export default class Contributor extends Component {
                     </div>
                 </div>
             </section>
+        )
+    }
+}
+
+class ContributorHappier extends Component {
+    render() {
+        return (
+            <div className="tile is-parent is-4">
+                <article className="tile is-child notification is-success has-text-left stat-tile">
+                    <p className="title">
+                        <span className="fa fa-smile-o"/>
+                    </p>
+                    <p className="subtitle is-3">Be happier</p>
+                    <p className="subtitle is-5">
+                        Gratitude scientifically proven to make people healthier and happier in their daily lives.<br/>
+                    </p>
+                </article>
+            </div>
+        )
+    }
+}
+
+class ContributorStandFor extends Component {
+    render() {
+        return (
+            <div className="tile is-parent is-4">
+                <article className="tile is-child notification is-success has-text-left stat-tile">
+                    <p className="title">
+                        <span className="fa fa-paint-brush"/>
+                    </p>
+                    <p className="subtitle is-3">Give freedom</p>
+                    <p className="subtitle is-5">
+                        Art is a social good, it affects our heart and heals our souls.<br/>
+                        We are the people, who this art is made for, we need it in our lives, more then we need things.
+                        We can not only take, but we also can give back to the artist a small token of our gratitude.
+                    </p>
+                </article>
+            </div>
+        )
+    }
+}
+
+class ContributorNotMuch extends Component {
+    render() {
+        return (
+            <div className="tile is-parent is-4">
+                <article className="tile is-child notification is-success has-text-left stat-tile">
+                    <p className="title">
+                        <span className="fa fa-balance-scale"/>
+                    </p>
+                    <p className="subtitle is-3">It does not take much</p>
+                    <p className="subtitle is-5">
+                        You can afford it.<br/>
+                        The connection that you get to the artist is so much more. Try it, feel it, embrace it.
+                    </p>
+                </article>
+            </div>
+        )
+    }
+}
+
+class ContributorPoints extends Component {
+    render() {
+        return (
+            <div className="tile is-ancestor">
+                <ContributorStandFor/>
+                <ContributorNotMuch/>
+                <ContributorHappier/>
+            </div>
+        )
+    }
+}
+
+
+export default class Contributor extends Component {
+    render() {
+        return (
+            <div>
+                <ComponentWrap>
+                    <ContributorHero/>
+                </ComponentWrap>
+                <ComponentWrap>
+                    <ContributorPoints/>
+                </ComponentWrap>
+            </div>
         );
     }
 }
--- app/components/landing/Creator.jsx
+++ app/components/landing/Creator.jsx
@@ -1,26 +1,111 @@
 import React, {Component} from "react";
+import ComponentWrap from "../ComponentWrap";
 
-export default class Creator extends Component {
+class CreatorHow extends Component {
     render() {
         return (
-            <section className="hero">
+            <div className="tile is-parent is-4">
+                <article className="tile is-child notification is-success has-text-left stat-tile">
+                    <p className="title">
+                        <span className="fa fa-handshake-o"/>
+                    </p>
+                    <p className="subtitle is-3">Social contract</p>
+                    <p className="subtitle is-5">
+                        Tipping is a widely present in our daily life.
+                        But there are so many questions - how much to tip? when to tip? how to tip?<br/>
+                        We are creating a social contract, that would be clear to everyone.<br/>
+                    </p>
+                </article>
+            </div>
+        )
+    }
+}
+
+class CreatorWhyUs extends Component {
+    render() {
+        return (
+            <div className="tile is-parent is-4">
+                <article className="tile is-child notification is-success has-text-left stat-tile">
+                    <p className="title">
+                        <span className="fa fa-heart"/>
+                    </p>
+                    <p className="subtitle is-3">Freedom to create more</p>
+                    <p className="subtitle is-5">
+                        Art is here to change the world.<br/>
+                        It's hard, it takes all of your time, but.<br/>
+                        <strong>You'll share it regardless.</strong><br/>
+                        We are here to help you focus on your art and take care of your bills.
+                    </p>
+                </article>
+            </div>
+        )
+    }
+}
+
+class CreatorHowYouGetPaid extends Component {
+    render() {
+        return (
+            <div className="tile is-parent is-4">
+                <article className="tile is-child notification is-success has-text-left stat-tile">
+                    <p className="title">
+                        <span className="fa fa-credit-card-alt"/>
+                    </p>
+                    <p className="subtitle is-3">How you get paid</p>
+                    <p className="subtitle is-5">
+                        LoveIt handles all of the payments, <strong>you never have to deal with integration nightmare.</strong> Every click turns into money on your bank account.
+                    </p>
+                </article>
+            </div>
+        )
+    }
+}
+
+class CreatorHero extends Component {
+    render() {
+        return (
+            <section className="hero is-primary is-large header-image creator-image">
                 <div className="has-text-centered">
-                    <div className="columns is-vcentered">
-                        <div className="column">
-                            <h1 className="title is-2">
-                                Creator
+                    <div className="hero-body">
+                        <div className="container has-text-centered">
+                            <h1 className="title is-1">
+                                Artist
                             </h1>
-                            <h2 className="subtitle is-5">
-                                Create and share your love.<br/>
-                                Think about your art, not bills<br/>
+                            <h2 className="subtitle is-3">
+                                Focus on your art, not bills<br/>
                             </h2>
-                        </div>
-                        <div className="column ">
-                            <img src="/images/creator.jpg"></img>
                         </div>
                     </div>
                 </div>
             </section>
+        )
+    }
+}
+
+class CreatorPoints extends Component {
+    render() {
+        return (
+            <div>
+                <div className="tile is-ancestor">
+                    <CreatorWhyUs/>
+                    <CreatorHow/>
+                    <CreatorHowYouGetPaid/>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default class Creator extends Component {
+    render() {
+        return (
+            <div>
+                <ComponentWrap>
+                    <CreatorHero/>
+                </ComponentWrap>
+                <ComponentWrap>
+                    <CreatorPoints/>
+                </ComponentWrap>
+            </div>
         );
     };
 };
