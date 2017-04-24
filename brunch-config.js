d9fd4b0b371e35274372160f7649229dc99b0f1d
Anton Oparin <mavarazy@gmail.com>
2017-04-24 11:37:26 +0300

    Adding gzip to brunch

--- brunch-config.js
+++ brunch-config.js
@@ -1,19 +1,31 @@
 // See http://brunch.io for documentation.
 exports.files = {
-  npm: {
-      styles: {
-          "bulma": ['css/bulma.css'],
-          "font-awesome": ['css/font-awesome.css']
-      }
-  },
-  javascripts: {
-    joinTo: 'app.js'
-  },
-  stylesheets: { joinTo: 'app.css' }
+    npm: {
+        styles: {
+            "bulma": ['css/bulma.css'],
+            "font-awesome": ['css/font-awesome.css']
+        }
+    },
+    javascripts: {
+        joinTo: 'app.js'
+    },
+    stylesheets: {
+        joinTo: 'app.css'
+    }
 };
 
 exports.plugins = {
-  babel: {presets: ['latest', 'react']}
+    babel: {
+        presets: ['latest', 'react']
+    },
+    gzip: {
+        paths: {
+            javascript: '/',
+            stylesheet: '/',
+        },
+        removeOriginalFiles: false,
+        renameGzipFilesToOriginalFiles: false,
+    }
 };
 
 exports.hot = true;
