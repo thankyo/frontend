821f4ec89f98fac1e3f1320c7329a35906ae1c25
Anton Oparin <mavarazy@gmail.com>
2017-05-08 11:47:30 +0300

    Clean state on logout

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -3,9 +3,24 @@ import user from "./user";
 import auth from "./auth";
 import payment from './payment';
 import thank from "./thank";
+import lastAction from './lastAction';
 import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
-import { reducer as formReducer } from 'redux-form'
+import { reducer as formReducer } from 'redux-form';
+
+import {LOGOUT} from './auth.actions';
+
+function cleanOnLogout(reducer) {
+    return function (state, action) {
+        switch (action.type) {
+            case LOGOUT:
+                return reducer({}, action);
+            default:
+                return reducer(state, action);
+        }
+    }
+};
+
 
 export default function(){
     return combineReducers(
@@ -13,10 +28,11 @@ export default function(){
             routing,
             auth,
             menu,
-            user,
-            payment,
-            thank,
-            form: formReducer
+            user: cleanOnLogout(user),
+            payment: cleanOnLogout(payment),
+            thank: cleanOnLogout(thank),
+            form: cleanOnLogout(formReducer),
+            lastAction,
         }
     );
 }
