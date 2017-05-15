ab65dd42638dccfcb2a69847f8ba7f8cfbcfed3d
Anton Oparin <mavarazy@gmail.com>
2017-05-15 15:53:48 +0300

    Updating view

--- app/components/landing/creator/Hero.jsx
+++ app/components/landing/creator/Hero.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Brand from "../../Brand";
 import {Link} from "react-router";
+import FacebookLogin from '../FacebookLogin';
 
 class CreatorHead extends Component {
     render() {
@@ -27,7 +28,7 @@ class CreatorHead extends Component {
 export default class Hero extends Component {
     render() {
         return (
-            <section className="hero is-large is-success">
+            <section className="hero is-medium is-success">
                 <div className="has-text-centered">
                     <CreatorHead/>
                     <div className="hero-body">
@@ -38,6 +39,11 @@ export default class Hero extends Component {
                             <h2 className="subtitle is-3">
                                 Connect to your most loyal fans<br/>
                             </h2>
+                            <FacebookLogin>
+                                <div className="button is-info is-outlined is-hovered is-large">
+                                    <span>Just try</span>
+                                </div>
+                            </FacebookLogin>
                         </div>
                     </div>
                 </div>
