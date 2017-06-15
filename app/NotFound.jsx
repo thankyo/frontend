29448498efffc2647924e5a7f26e4a7003e0681c
Anton Oparin <mavarazy@gmail.com>
2017-06-15 17:17:11 +0300

    Updating not found

--- app/NotFound.jsx
+++ app/NotFound.jsx
@@ -3,6 +3,7 @@ import Navigation from "components/Navigation";
 
 export default class NotFound extends Component {
     render() {
+        let emailContact = `mailto:antono@loveit.tips?subject=404 on ${location.host}/${location.pathname}`
         return (
             <div>
                 <Navigation/>
@@ -10,6 +11,7 @@ export default class NotFound extends Component {
                     <div className="container has-text-centered">
                         <p className="title">404</p>
                         <p className="subtitle">You can be proud of yourself, you broke it.</p>
+                        If this is something important <a href={emailContact}>contact us</a>, we'll take a look.
                     </div>
                 </div>
             </div>
