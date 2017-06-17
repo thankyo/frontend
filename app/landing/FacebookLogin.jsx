d51bd631a7196bcbf925d452ba3512b242898e26
Anton Oparin <mavarazy@gmail.com>
2017-06-18 00:42:26 +0300

    #20 fixing redirect URL

--- app/landing/FacebookLogin.jsx
+++ app/landing/FacebookLogin.jsx
@@ -2,8 +2,9 @@ import React, { Component } from "react";
 
 export default class FacebookLogin extends Component {
     render() {
-        let redirect = `http://${window.location.host}/auth/facebook`
+        let protocol = process.env.NODE_ENV === 'production' ? `https` : `http`;
         let clientId = process.env.NODE_ENV !== 'production' ? `1867505463531006` : `1429718427098411`;
+        let redirect = `${protocol}://${window.location.host}/auth/facebook`;
         let url = `https://graph.facebook.com/v2.3/oauth/authorize?scope=email&client_id=${clientId}&redirect_uri=${redirect}&response_type=code`
         return (
             <div onClick={() => window.location = url}>
