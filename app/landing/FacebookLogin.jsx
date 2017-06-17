e45efb2c0187fdd1d2bf2abd7720e078a4057344
Anton Oparin <mavarazy@gmail.com>
2017-06-18 00:35:49 +0300

    #20

--- app/landing/FacebookLogin.jsx
+++ app/landing/FacebookLogin.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export default class FacebookLogin extends Component {
     render() {
         let redirect = `http://${window.location.host}/auth/facebook`
-        let clientId = `1867505463531006`;
+        let clientId = `1429718427098411`;
         let url = `https://graph.facebook.com/v2.3/oauth/authorize?scope=email&client_id=${clientId}&redirect_uri=${redirect}&response_type=code`
         return (
             <div onClick={() => window.location = url}>
