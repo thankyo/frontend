b5f18f973cb7c91520764268aea269fe608de5ce
Anton Oparin <mavarazy@gmail.com>
2017-07-31 18:49:24 +0200

    Ups I did it again

--- app/landing/FacebookLogin.jsx
+++ app/landing/FacebookLogin.jsx
@@ -4,7 +4,7 @@ export default class FacebookLogin extends Component {
     render() {
         let clientId = process.env.NODE_ENV === 'production' ? 1429718427098411 : 1867505463531006;
         let { host, protocol } = window.location;
-        let url = `https://graph.facebook.com/v2.3/oauth/authorize?scope=email&client_id=${clientId}&redirect_uri=${protocol}://${host}/auth/facebook&response_type=code`;
+        let url = `https://graph.facebook.com/v2.3/oauth/authorize?scope=email&client_id=${clientId}&redirect_uri=${protocol}//${host}/auth/facebook&response_type=code`;
         return (
             <div onClick={() => window.location = url}>
                 {this.props.children}
