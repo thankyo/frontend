4477735c1e4d8cb5c0a9b53707bb85d6dfd632b0
Anton Oparin <mavarazy@gmail.com>
2017-07-31 17:30:12 +0200

    Fixing login URL

--- app/landing/FacebookLogin.jsx
+++ app/landing/FacebookLogin.jsx
@@ -3,7 +3,8 @@ import React, { Component } from "react";
 export default class FacebookLogin extends Component {
     render() {
         let clientId = process.env.NODE_ENV === 'production' ? 1429718427098411 : 1867505463531006;
-        let url = `https://graph.facebook.com/v2.3/oauth/authorize?scope=email&client_id=${clientId}&redirect_uri=http://${window.location.host}/auth/facebook&response_type=code`;
+        let { host, protocol } = window.location;
+        let url = `https://graph.facebook.com/v2.3/oauth/authorize?scope=email&client_id=${clientId}&redirect_uri=${protocol}://${host}/auth/facebook&response_type=code`;
         return (
             <div onClick={() => window.location = url}>
                 {this.props.children}
