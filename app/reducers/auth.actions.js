24ae54ae865cbce801baf0b40f27f55fcca450cb
Anton Oparin <mavarazy@gmail.com>
2017-07-31 15:03:10 +0200

    Switching to page loader instead of custom gifs

--- app/reducers/auth.actions.js
+++ app/reducers/auth.actions.js
@@ -1,6 +1,7 @@
 import 'whatwg-fetch';
 import authService from '../service/auth';
 import { typeEvent, dispatchPromise } from '../service/promiseStates';
+import { Link, browserHistory } from 'react-router';
 
 export const LOGOUT = "LOGOUT";
 
@@ -13,12 +14,19 @@ export function logout() {
 
 export const FACEBOOK_LOGIN = "FACEBOOK_LOGIN";
 
+function authWithFacebook(dispatch, search) {
+  if (!authService.isAuthenticated()) {
+    let url = `/api/v1/auth/authenticate/facebook${search}`;
+    let p = fetch(new Request(url)).then((res) => res.json());
+    return dispatchPromise(p, FACEBOOK_LOGIN, dispatch)
+  } else {
+    return Promise.resolve();
+  }
+}
+
 export function authFacebook(search) {
     return (dispatch) => {
-        if (!authService.isAuthenticated()) {
-            let url = `/api/v1/auth/authenticate/facebook${search}`;
-            let p = fetch(new Request(url)).then((res) => res.json());
-            dispatchPromise(p, FACEBOOK_LOGIN, dispatch);
-        }
+        authWithFacebook(dispatch, search).
+        then(() => browserHistory.push("/supporter/my"));
     }
 }
