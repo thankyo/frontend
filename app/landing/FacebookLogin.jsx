8756338118f6e5da5c1a6f4891d50f0094354a57
Anton Oparin <mavarazy@gmail.com>
2017-07-31 15:08:25 +0200

    Updating facebook url builder

--- app/landing/FacebookLogin.jsx
+++ app/landing/FacebookLogin.jsx
@@ -2,9 +2,8 @@ import React, { Component } from "react";
 
 export default class FacebookLogin extends Component {
     render() {
-        let url = process.env.NODE_ENV === 'production' ?
-            `https://graph.facebook.com/v2.3/oauth/authorize?scope=email&client_id=1429718427098411&redirect_uri=https://loveit.tips/auth/facebook&response_type=code` :
-            `https://graph.facebook.com/v2.3/oauth/authorize?scope=email&client_id=1867505463531006&redirect_uri=http://${window.location.host}/auth/facebook&response_type=code`
+        let clientId = process.env.NODE_ENV === 'production' ? 1429718427098411 : 1867505463531006;
+        let url = `https://graph.facebook.com/v2.3/oauth/authorize?scope=email&client_id=${clientId}&redirect_uri=http://${window.location.host}/auth/facebook&response_type=code`;
         return (
             <div onClick={() => window.location = url}>
                 {this.props.children}
