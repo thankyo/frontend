059619b7cf110fde1ef86171587f14f1c009c877
Anton Oparin <mavarazy@gmail.com>
2017-08-02 12:47:11 +0200

    Fixing load error

--- app/conf/sentry.js
+++ app/conf/sentry.js
@@ -1,16 +1,5 @@
 import { loadScriptAsPromise } from "./loadScript";
 
-function loadSentry(callback) {
-  let elem = document.createElement('script');
-  elem.src = "https://cdn.ravenjs.com/3.17.0/raven.min.js";
-  elem.async = true;
-  elem.type = "text/javascript";
-  elem.onload = callback;
-  let scpt = document.getElementsByTagName('script')[0];
-  scpt.parentNode.insertBefore(elem, scpt);
-}
-
-
 export default function () {
   return loadScriptAsPromise("https://cdn.ravenjs.com/3.17.0/raven.min.js").then(() => {
     let install = Raven.config('https://9135be526dd647ab9aaff18d0871fc82@sentry.io/197694').install()
