77ca1dd33de6ca35ea0c738b0dea525b9245f55b
Anton Oparin <mavarazy@gmail.com>
2017-08-03 11:52:15 +0200

    Fixing consent form load

--- app/conf/consent.js
+++ app/conf/consent.js
@@ -2,25 +2,26 @@ import { loadScriptAsPromise } from './loadScript';
 import { loadCSS } from './loadCSS';
 
 function doConfigure() {
-  window.cookieconsent.initialise({
-    "palette": {
-      "popup": {
-        "background": "#eaf7f7",
-        "text": "#5c7291"
+  if (window.cookieconsent) {
+    window.cookieconsent.initialise({
+      "palette": {
+        "popup": {
+          "background": "#eaf7f7",
+          "text": "#5c7291"
+        },
+        "button": {
+          "background": "#56cbdb",
+          "text": "#ffffff"
+        }
       },
-      "button": {
-        "background": "#56cbdb",
-        "text": "#ffffff"
-      }
-    },
-    "theme": "edgeless"
-  })
-
+      "theme": "edgeless"
+    })
+  }
 }
 
 export default function configure() {
   if (document.cookie.indexOf("cookieconsent_status=dismiss") === -1) {
-    let loadScript = loadScriptAsPromise("//cdn.jsdelivr.net/cookieconsent3/3.0.4/cookieconsent.min.js", doConfigure);
+    let loadScript = loadScriptAsPromise("//cdn.jsdelivr.net/cookieconsent3/3.0.4/cookieconsent.min.js").then(doConfigure);
     let loadCSS = loadCSS("//cdn.jsdelivr.net/cookieconsent3/3.0.4/cookieconsent.min.css", "/css/cookieconsent.min.css");
     return Promise.all([loadScript, loadCSS]);
   } else {
