d92c5c10c0cc80f321a4bbb51cfc12659e44ff76
Anton Oparin <mavarazy@gmail.com>
2017-08-03 11:10:34 +0200

    Adding assign polypill

--- app/conf/index.js
+++ app/conf/index.js
@@ -2,11 +2,14 @@
 import analytics from './analytics';
 import consent from './consent';
 import loadCSS from './loadCSS';
+import polyfills from './polyfills';
 import sentry from "./sentry";
+
 
 export default function configure(history, store) {
   analytics(history);
   //sentry();
+  polyfills;
   return Promise.all([
     loadCSS(),
     consent()
