f86ad56371f4f29c8e1362976d441333c88f3af0
Anton Oparin <mavarazy@gmail.com>
2017-09-02 18:43:28 +0300

    Updating invite

--- src/pages/supporter/Invite.js
+++ src/pages/supporter/Invite.js
@@ -1,30 +1,32 @@
 import React from "react";
 import { invite } from "../../reducers/invite.actions";
-import Icon from "../components/Icon";
 import { connect } from 'react-redux';
 import { Field, reduxForm } from "redux-form";
 
-let Invite = ({ handleSubmit }) => {
+function Invite({ handleSubmit }) {
   return (
-    <div className="has-text-centered notification is-info">
-      <form onSubmit={ handleSubmit }>
-        <h1 className="title is-4">Who you are ready to support?</h1>
-        <h2 className="subtitle">Put a link or email of the creator and we'll contact him</h2>
-        <div className="field has-addons">
-          <p className="control is-expanded has-icons-left">
-            <Field name="linkOrEmail" component="input" type="text" className="input" placeholder="link or email"/>
-            <span className="icon is-small is-left">
-              <i className="fa fa-globe"></i>
-            </span>
-          </p>
-          <p className="control has-icon-left">
-            <button className="button is-info is-inverted is-outlined is-hovered" type="submit">
-              <Icon fa="hand-spock-o" text="Invite"/>
-            </button>
-          </p>
-        </div>
-      </form>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div className="support-block has-text-centered notification">
+        <form>
+          <h1 className="title is-5">Who you are ready to support?</h1>
+          <h2 className="subtitle is-6">Put a link or email of the creator and we'll contact him</h2>
+          <div className="field has-addons">
+            <p className="control is-expanded has-icons-left">
+              <Field name="linkOrEmail" component="input" type="text" className="input" placeholder="link or email"/>
+              <span className="icon is-small is-left">
+                  <i className="fa fa-globe"></i>
+                </span>
+            </p>
+            <p className="control has-icon-left">
+              <button className="button button-green" type="submit"><span className="is-narrow"><span
+                className="icon">
+                  <i className="fa fa-hand-peace-o"></i></span><span>Invite</span></span>
+              </button>
+            </p>
+          </div>
+        </form>
+      </div>
+    </form>
   )
 };
 
@@ -33,7 +35,7 @@ Invite = reduxForm({
 })(Invite);
 
 const InviteToReducer = ({ invite }) => {
-  return (<Invite onSubmit={ invite }/>)
+  return (<Invite onSubmit={invite}/>)
 };
 
 const mapDispatchToProps = (dispatch) => {
