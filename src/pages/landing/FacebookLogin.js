1cf143faed5f8c416aa97cd8acc0486511c5a412
Anton Oparin <mavarazy@gmail.com>
2017-09-17 19:48:40 +0300

    Updating URL

--- src/pages/landing/FacebookLogin.js
+++ src/pages/landing/FacebookLogin.js
@@ -4,7 +4,7 @@ export default class FacebookLogin extends Component {
     render() {
         let clientId = FACEBOOK_KEY;
         let { host, protocol } = window.location;
-        let url = `https://graph.facebook.com/v2.3/oauth/authorize?scope=email&client_id=${clientId}&redirect_uri=${protocol}//${host}/auth/facebook&response_type=code`;
+        let url = `https://graph.facebook.com/v2.9/oauth/authorize?scope=email&client_id=${clientId}&redirect_uri=${protocol}//${host}/auth/facebook&response_type=code`;
         return (
             <div onClick={() => window.location = url}>
                 {this.props.children}
