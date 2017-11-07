223222b4c128426f3c829e53645e862e69586396
Anton Oparin <mavarazy@gmail.com>
2017-11-07 15:24:11 +0300

    Adding loading prototype

--- src/common/Loading.js
+++ src/common/Loading.js
@@ -1,4 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
+
+export function loadingComponent(loader, Component) {
+  class LoadingComponent extends Component {
+    constructor(props) {
+      super(props);
+
+      this.state = { loading: true }
+    }
+    componentWillMount() {
+      loader().then(() => this.setState({ loading: false }));
+    }
+    render() {
+      if (this.state.loading) {
+        return <Loading/>
+      } else {
+        return <Component/>
+      }
+    }
+  }
+
+  return LoadingComponent;
+}
 
 export default function Loading() {
   return (
