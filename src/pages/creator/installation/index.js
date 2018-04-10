a23625e7e2d276feb21716196bd36f233fdc489b
Anton Oparin <mavarazy@gmail.com>
2018-04-10 21:09:12 +0700

    #42 Adding tumblr to project page

--- src/pages/creator/installation/index.js
+++ src/pages/creator/installation/index.js
@@ -30,9 +30,17 @@ function UserProjects({ pending, installed }) {
 
 function GoogleStatus({ connected, url }) {
   if (connected) {
-    return <a className="button is-small" disabled><CheckedIcon>Google Connected</CheckedIcon></a>
+    return <a className="button is-small is-disabled" disabled><CheckedIcon>Google Connected</CheckedIcon></a>
   } else {
     return <a className="button is-small is-primary" href={url}><GoogleIcon>Connect Google</GoogleIcon></a>
+  }
+}
+
+function TumblrStatus({ connected, url }) {
+  if (connected) {
+    return <a className="button is-small is-disabled" disabled><CheckedIcon>Tumblr Connected</CheckedIcon></a>
+  } else {
+    return <a className="button is-small is-primary" href={url}><GoogleIcon>Connect Tumblr</GoogleIcon></a>
   }
 }
 
@@ -42,32 +50,44 @@ function ResourceOwnershipStatus({ connected, owned }) {
   }
 
   return owned.length === 0
-    ? <div className="is-small">Google does not have any site data, please refer to <a href="https://support.google.com/webmasters/answer/35179?hl=en">this</a> article or contact us.</div>
-    : <div className="is-size-7">We see <strong>{owned.join(", ")}</strong> from Google</div>
+    ? <div className="is-small">We do not see any site data, please contact us, we are here to help.</div>
+    : <div className="is-size-7">We see <strong>{owned.join(", ")}</strong></div>
 }
 
-function InstallationPage({ pending, owned, installed, isLoading, isGoogleConnected, googleUrl }) {
+function InstallationPage({ pending, owned, installed, isLoading, google, tumblr }) {
   return (
     <Fragment>
       <h1 className="subtitle">Installation</h1>
-      <h6 className="subtitle is-6">We are using <strong>Google Verification</strong> to verify your site ownership.</h6>
-      <GoogleStatus connected={isGoogleConnected} url={googleUrl}/>
+      <h6 className="subtitle is-6">We are using <strong>Google Verification</strong> & <strong>Tumblr</strong> to verify your site ownership.</h6>
+      <div className="field has-addons">
+        <p className="control">
+          <GoogleStatus {... google}/>
+        </p>
+        <p className="control">
+          <TumblrStatus {... tumblr}/>
+        </p>
+      </div>
       <br/>
-      <br/>
-      {!isLoading && <ResourceOwnershipStatus connected={isGoogleConnected} owned={owned}/>}
+      {!isLoading && <ResourceOwnershipStatus connected={google.connected || tumblr.connected} owned={owned}/>}
       <br/>
       {isLoading ? <Spinner/> : <UserProjects pending={pending} installed={installed}/>}
     </Fragment>
   );
 }
 
-const mapStateToProps = ({ project: { owned, byId }, user: { my : { data: { profiles = {} } } }, auth: { url: { google }} } ) => ({
+const mapStateToProps = ({ project: { owned, byId }, user: { my : { data: { profiles = {} } } }, auth: { url } } ) => ({
   installed: owned.installed.map(id => byId[id]),
   owned: owned.owned || [],
   pending: owned.pending.map(url => ({ url })),
   isLoading: owned.isLoading,
-  isGoogleConnected: profiles.google,
-  googleUrl: google
+  google: {
+    connected: profiles.google,
+    url: url.google
+  },
+  tumblr: {
+    connected: profiles.tumblr,
+    url: url.tumblr
+  },
 });
 
 const mapDispatchToProps = (dispatch) => {
