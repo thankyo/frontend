f29148d9fe914a4d9910882481f1c685471613a1
Anton Oparin <mavarazy@gmail.com>
2018-04-29 11:35:52 +0700

    #46 Updating Tumblr manual

--- src/pages/creator/installation/pending/instruction/Tumblr.js
+++ src/pages/creator/installation/pending/instruction/Tumblr.js
@@ -2,13 +2,10 @@ import React from "react";
 
 const TumblrInstruction = () => (
   <div>
-    Tumblr allows 3rd party services like ours, to modify new & existing posts.<br/>
-    You already allowed us to make modifications on your posts, when signed up with Tumblr.<br/>
-    So all you need to do is finish this installation.<br/>
+    The best way to connect to Tumblr is, by using Tumblr integration.
     <br/>
-    We are not doing any modifications, of your posts without you knowing.<br/>
     <br/>
-    We support only <strong>photo</strong> posts as of the moment, we'll be adding more post types soon.
+    Just connect your account, and we'll take it from there.
     <br/>
     <br/>
     <a href="http://tumblr.loveit.tips" target="_blank" className="is-small">example</a>
